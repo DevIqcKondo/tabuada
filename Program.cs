f8a6219d5e4d1f10079ea59d67a278e5eb52233f
Isadora Queiroz <KondoIqcDev@gmail.com>
2021-04-28 12:03:59 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int resultado;
+            Console.Write("Digite um número para contruir sua tabuada: ");
+            numero=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= 10; i++){
+                resultado=numero*i;
+                Console.WriteLine(resultado);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # tabuada
-Código que exibe a tabuada do número que o usuário informar.
+Código que exibe a tabuada do número que o usuário digitar.
